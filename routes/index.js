70b18c11b75522b96679178d4cdc31ef200aef9e
jarenfanshawe <j_mcdonald24@fanshaweonline.ca>
2018-02-09 11:29:29 -0500

    adding ejs files

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+	{ name: "Jade", usefulness : "sucky" },
+	{ name: "EJS", usefulness : "a little better" },
+	{ name: "Handlebars", usefulness : "awesome" }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   	res.render('index', {
 		title: 'Express',
-		description: "sucks!",
-		method: "easy!"
+		description: "better!",
+		method: "easy",
+		templates: templateTypes
 	});
 });
 
